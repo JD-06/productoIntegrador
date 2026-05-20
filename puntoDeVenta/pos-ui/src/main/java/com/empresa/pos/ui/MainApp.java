48e534b1ba25bc50ddb20bf9580f18a22d7672a2
JuJo-SC <juanjosesantiagocruz635@gmail.com>
2026-05-20 01:26:34 -0600

    feat: implement Login UI and navigation (MVC)

--- puntoDeVenta/pos-ui/src/main/java/com/empresa/pos/ui/MainApp.java
+++ puntoDeVenta/pos-ui/src/main/java/com/empresa/pos/ui/MainApp.java
@@ -10,10 +10,10 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main_pos.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
         Parent root = loader.load();
         
-        Scene scene = new Scene(root, 1280, 800);
+        Scene scene = new Scene(root, 800, 600);
         scene.getStylesheets().add(getClass().getResource("/css/design-system.css").toExternalForm());
         
         primaryStage.setTitle("POS Empresarial ERP");
